2139a61db64e66a65d403981ef50f74af3b558f2
Diego Rey <holacinema@hotmail.com>
2022-11-21 02:37:52 -0300

    Clase 04 completada

--- script.js
+++ script.js
@@ -1,27 +1,53 @@
-const btn = document.querySelector("[data-form-btn]");
-console.log(btn);
-
-const createTask = (evento) => {
-    evento.preventDefault();
-    const input = document.querySelector("[data-form-input]");
-    value = input.value;
-    const list = document.querySelector("[data-list]");
-    const task = document.createElement("li");
-    task.classList.add("card")
-    input.value = "";
-    const content  = `
-    <div>
-      <i class="far fa-check-square icon"></i>
-      <span class="task">${value}</span>
-    </div>
-    <i class="fas fa-trash-alt trashIcon icon"></i>`;
-    task.innerHTML = content;
-    console.log(content);
-
-    list.appendChild(task);
-};
-
-//arrow function
-/* En vez de poner
-btn.addEventListener("click", function(evento){...}*/
-btn.addEventListener("click", createTask);
+( () =>{
+    const btn = document.querySelector("[data-form-btn]");
+    console.log(btn);
+    
+    const createTask = (evento) => {
+        //Previene que se recargue la pagina
+        evento.preventDefault();
+        const input = document.querySelector("[data-form-input]");
+        //Guardamos el valor de input en una variable
+        value = input.value;
+        const list = document.querySelector("[data-list]");
+        //Creamos un elemento li que lo guardamos en task
+        const task = document.createElement("li");
+        //Agregamos a task la clase card
+        task.classList.add("card")
+        //Borramos el valor de input
+        input.value = "";
+        const taskContent = document.createElement("div")
+        //A taskContent le agregamos lo que retorna la funcion
+        taskContent.appendChild(checkComplete());
+        const titleTask = document.createElement("span");
+        titleTask.classList.add("task");
+        titleTask.innerText = value;
+        //Agregamos titleTask a taskContent
+        taskContent.appendChild(titleTask);
+        const content  = `<i class="fas fa-trash-alt trashIcon icon"></i>`;
+        //task.innerHTML = content;
+        task.appendChild(taskContent);
+        list.appendChild(task);
+    
+        console.log(content);
+    };
+    
+    //arrow function
+    /* En vez de poner
+    btn.addEventListener("click", function(evento){...}*/
+    btn.addEventListener("click", createTask);
+    
+    const checkComplete = () =>{
+        const i = document.createElement("div");
+        i.classList.add("far", "fa-check-square", "icon");
+        i.addEventListener("click", completeTask);
+        return i;
+    };
+    
+    const completeTask = (event) =>{
+        const element = event.target;
+        // En vez de add pongo toggle. Si la clase existe la remueve, sino la agrega.
+        element.classList.toggle("fas");
+        element.classList.toggle("complete-icon");
+        element.classList.toggle("far");
+    };
+})();

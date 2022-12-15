5bb69af1232a71a2ddf75d5252bacca3ed9f3ddd
Diego Rey <holacinema@hotmail.com>
2022-12-15 09:56:00 -0300

    Se separó funcionalidades en 2 funciones

--- script.js
+++ script.js
@@ -4,13 +4,21 @@ import deleteIcon from "./components/deleteIcon.js";
 const btn = document.querySelector("[data-form-btn]");
 console.log(btn);
 
+const addTask = (evento) => {
+    const list = document.querySelector("[data-list]");
+    const task = createTask(evento)
+    list.appendChild(task);
+}
+
 const createTask = (evento) => {
     //Previene que se recargue la pagina
     evento.preventDefault();
     const input = document.querySelector("[data-form-input]");
+    const calendar = document.querySelector("[data-form-date]");
     //Guardamos el valor de input en una variable
     const value = input.value;
-    const list = document.querySelector("[data-list]");
+    const date = calendar.value;
+    const dateFormat = moment(date).format("DD/MM/YYYY");
     //Creamos un elemento li que lo guardamos en task
     const task = document.createElement("li");
     //Agregamos a task la clase card
@@ -26,12 +34,16 @@ const createTask = (evento) => {
     //Agregamos titleTask a taskContent
     taskContent.appendChild(titleTask);
     //task.innerHTML = content;
+    const dateElement = document.createElement("span");
+    dateElement.innerHTML = dateFormat;
     task.appendChild(taskContent);
+    //Agregamos elemento fecha
+    task.appendChild(dateElement);
     task.appendChild(deleteIcon());
-    list.appendChild(task);
+    return task;
 }
 
 //arrow function
 /* En vez de poner
 btn.addEventListener("click", function(evento){...}*/
-btn.addEventListener("click", createTask);
+btn.addEventListener("click", addTask);
